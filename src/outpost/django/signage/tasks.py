23ad57b71badf16519726b20eecfeda82a725645
Michael Fladischer <michael@fladi.at>
2023-02-16 23:04:55 +0100

    Add ScheduleItem cleanup task.

--- src/outpost/django/signage/tasks.py
+++ src/outpost/django/signage/tasks.py
@@ -1,4 +1,20 @@
 import logging
 from datetime import timedelta
 
+import isodate
+from celery import shared_task
+from django.utils import timezone
+
+from . import models
+
 logger = logging.getLogger(__name__)
+
+
+class ScheduleTask:
+    @shared_task(bind=True, ignore_result=True, name=f"{__name__}.Schedule:cleanup")
+    def cleanup(task, interval: str) -> None:
+        threshold = timezone.now() - isodate.parse_duration(interval)
+        for si in models.ScheduleItem.objects.exclude(range__endswith=None).filter(
+            range__fully_lt=threshold
+        ):
+            si.delete()
